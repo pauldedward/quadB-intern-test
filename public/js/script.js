78ac6604f3997a3b3a46b8ea576a42e56c3bf512
pauldedward <edward2000ed@gmail.com>
2021-07-23 00:11:11 +0530

    timer calculation done

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,17 @@
 console.log("hi");
+
+const circle=document.querySelector('circle');
+
+let maxOffset = 2*Math.PI*circle.r.baseVal.value;
+
+circle.setAttribute("stroke-dasharray", maxOffset);
+offset = 0;
+const interval = setInterval(() => {
+        circle.setAttribute("stroke-dashoffset", offset);
+        if(offset < -maxOffset) {
+            offset = 0;
+        }
+        else {
+            offset -= maxOffset/ 240;    
+        }
+    }, 250);
